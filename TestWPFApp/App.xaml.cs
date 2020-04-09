1c0e7d20dff95c47fcaf89ccca3aafa5667eebcb
Илья Казютин <gorgutz1@yandex.ru>
2020-04-09 12:35:57 +0300

    Добавьте файлы проекта.

---
+++ TestWPFApp/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestWPFApp
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void ApplicationStartup(object sender, StartupEventArgs e)
+        {
+            MessageBox.Show("App in motion");
+        }
+    }
+}
